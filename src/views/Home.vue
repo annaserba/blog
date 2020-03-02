a473f046c301d0f1da283e3b80b67858b011a08f
annaserba <60755945+annaserba@users.noreply.github.com>
2020-03-03 00:18:39 +0300

    only home with change view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="home">
-    <Feeds/>
+    <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        
+        <h1 class="display-1 font-weight-thin mb-4">Ann Serba</h1>
+        <h4 class="subheading">.Net Programmer and entrepreneur</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+  <v-container
+        class="fill-height" 
+         >
+        <v-row align="center"
+          justify="center"
+        >
+         <v-col class="text-center" cols="12">
+          <h2 class="display-1 font-weight-thin mb-4">About me</h2>
+          <p>I am a business automation web engineer. 
+            The area of my expertise covers the full stack .Net technologies. 
+            But I specialize in front-end technologies. 
+            I have extensive experience in fixing software bugs.
+            I work only remotely</p>
+          </v-col>
+        </v-row>
+      </v-container>
+    
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import Feeds from '@/components/Feeds/Feeds.vue'
-
-export default {
-  name: 'home',
-  components: {
-    Feeds
-  }
-}
-</script>
