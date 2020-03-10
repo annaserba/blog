e6609d639c706a102f5e000f45154e32bcf06b19
annaserba <60755945+annaserba@users.noreply.github.com>
2020-03-10 20:21:50 +0300

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,29 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-container
+      class="text-center"
+      fill-height
+      style="height: calc(100vh - 58px);"
+    >
+      <v-row align="center">
+        <v-col>
+          <h1
+            v-if="error.statusCode === 404"
+            class="display-2 primary--text"
+          >
+            {{ pageNotFound }}
+          </h1>
+          <h1 v-else>
+            {{ otherError }}
+          </h1>
+
+          <p>The page you were looking for does not exist</p>
+          <NuxtLink to="/">
+            Home page
+          </NuxtLink>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
