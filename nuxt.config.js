598540f35c9f514102a5efd70aa5f6d62a9b64fe
annaserba <60755945+annaserba@users.noreply.github.com>
2020-03-21 23:01:20 +0300

    add yandex-verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'yandex-verification', name: 'yandex-verification', content: '19317c80c22288c4' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
