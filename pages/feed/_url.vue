29fc4e7dd125939036d39f7c347314f73989de1e
annaserba <60755945+annaserba@users.noreply.github.com>
2020-07-15 11:02:31 +0300

    Revert "add description"
    This reverts commit 8bce8f07b8e5e487cfaac3d10363ac484414f991.

--- pages/feed/_url.vue
+++ pages/feed/_url.vue
@@ -180,10 +180,7 @@ export default {
   head () {
     return {
       titleTemplate: '%s - Anna Serba',
-      title: this.$route.params.url,
-      meta: [
-        { hid: 'description', name: 'description', content:this.model&&this.model.fields.description? this.model.fields.description:"" }
-      ]
+      title: this.$route.params.url
     }
   }
 }
